2e8e4cde91a98b38bbb6324a65a65d1da0978ef2
luivrz <lluivrz@gmail.com>
2025-03-27 13:41:43 -0300

    final commit
    navbar finalizada com responsividade

--- navbar/src/App.jsx
+++ navbar/src/App.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
 
   return (
     <>
-    <header>
-      <nav>
-        <a className='logo' href="/">NavBar</a>
-        <ul className='nav-list'>
-          <li><a href="/">Home</a></li>
-          <li><a href="/">About</a></li>
-          <li><a href="/">Services</a></li> 
-          <li><a href="/">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
+      <body>
+        <header>
+          <nav>
+            <a className="logo" href="/">NAVBAR</a>
+            <div
+              className={`mobile-menu ${isActive ? 'active' : ''}`}
+              onClick={handleClick}
+            >
+              <div className="line1"></div>
+              <div className="line2"></div>
+              <div className="line3"></div>
+            </div>
+            <ul className={`nav-list ${isActive ? 'active' : ''}`}>
+              <li><a href="#">Início</a></li>
+              <li><a href="#">Sobre</a></li>
+              <li><a href="#">Projetos</a></li>
+              <li><a href="#">Contato</a></li>
+            </ul>
+          </nav>
+        </header>
+        <main></main>
+      </body>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
