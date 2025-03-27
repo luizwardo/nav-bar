168d21d48fb4f133e162a72ad7dd0d4974d51f7b
luivrz <lluivrz@gmail.com>
2025-03-27 12:17:22 -0300

    declared items

---
+++ navbar/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <nav>
+        <a className='logo' href="/">NavBar</a>
+        <ul className='nav-list'>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">About</a></li>
+          <li><a href="/">Services</a></li> 
+          <li><a href="/">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    </>
+  )
+}
+
+export default App
